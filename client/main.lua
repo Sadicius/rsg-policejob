5e44e92c55d2f3ac450fc1c66316b130184bd783
RexShack <rexshackuk@gmail.com>
2022-12-28 19:14:01 +0000

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -173,7 +173,7 @@ RegisterNetEvent('police:client:SendToJail', function(time)
     isEscorted = false
     ClearPedTasks(PlayerPedId())
     DetachEntity(PlayerPedId(), true, false)
-    TriggerEvent("prison:client:Enter", time)
+    TriggerEvent('rsg-prison:client:Enter', time)
 end)
 
 RegisterNetEvent('police:client:SendPoliceEmergencyAlert', function()
